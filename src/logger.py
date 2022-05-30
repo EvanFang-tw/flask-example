6071c7479b69fd3c69f7ffd2a66e49895533f66b
Evan Fang <evanfang.hi@gmail.com>
2022-05-30 20:10:50 +0800

    Update logger.py

--- src/logger.py
+++ src/logger.py
@@ -4,7 +4,11 @@ import logging.config
 
 # load config
 logging.config.fileConfig("configs/logging.ini", disable_existing_loggers=False)
-
+# logging.basicConfig(
+#     stream=sys.stdout,
+#     format="%(asctime)s.%(msecs)03dZ [%(levelname)s] %(name)s: %(message)s",
+#     datefmt="%Y-%m-%dT%H:%M:%S",
+# )
 
 def getLogger(name):
     logger = logging.getLogger(name)
