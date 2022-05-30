ce9b56de8878ea7f61dd580cfd71a142e0a6bab8
Evan Fang <evanfang.hi@gmail.com>
2022-05-30 23:29:25 +0800

    Update logger.py

--- src/logger.py
+++ src/logger.py
@@ -5,7 +5,10 @@ import logging.config
 # load config
 logging.config.fileConfig("configs/logging.ini", disable_existing_loggers=False)
 # logging.basicConfig(
-#     stream=sys.stdout,
+#     handlers=[
+#         logging.StreamHandler(),
+#         logging.FileHandler("transfer.log", delay=False, mode="a", encoding="utf-8"),
+#     ],
 #     format="%(asctime)s.%(msecs)03dZ [%(levelname)s] %(name)s: %(message)s",
 #     datefmt="%Y-%m-%dT%H:%M:%S",
 # )
